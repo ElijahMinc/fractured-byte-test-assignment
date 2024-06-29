d3c82b1b37c8129990f66e8b38f572bcc24696eb
Ilya Protsenko <ilya.protsenko@nixsolutions.com>
2024-06-29 18:00:09 +0100

    Updated classname of file name button, updated text for invalid file size notification

--- src/features/CanvasPointDrawing/lib/helpers/showErrorInvalidFileSize.ts
+++ src/features/CanvasPointDrawing/lib/helpers/showErrorInvalidFileSize.ts
@@ -1,6 +1,5 @@
-import { fileSizeLimit } from '@shared/constants/max-file-size.constant';
 import { toast } from 'react-toastify';
 
 export const showErrorInvalidFileSize = () => {
-  toast.error(`Invalid file size. Valid size of file is ${fileSizeLimit}`);
+  toast.error(`Invalid file size. Valid size of file is 2mb`);
 };
